fafa914b40ff72cdc4f9e01d84d6096e87cf4388
myaffiliate1990 <myaffiliate1990@gmail.com>
2025-06-15 11:53:19 +0000

    test commit in theme.ts kanet 2 3malnaha 3

--- src/theme.ts
+++ src/theme.ts
@@ -5,7 +5,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#1976d3',
     },
     secondary: {
       main: '#dc004e',
